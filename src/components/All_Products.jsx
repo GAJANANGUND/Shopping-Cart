2b48f95cebc60775d2128afd8cc4e073bd618322
Gajanan Gund <gajanangund2000@gmail.com>
2025-10-15 18:38:14 +0530

    modified video player

--- src/components/All_Products.jsx
+++ src/components/All_Products.jsx
@@ -5,11 +5,11 @@ import VideoPlayer from './VideoPlayer'
 
 const All_Products = () => {
   const { products } = useContext(DataContext);
-  console.log("my products =", products);
+  // console.log("my products =", products);
   return (
     <>
       <VideoPlayer src={"https://www.apple.com/105/media/ww/iphone/family/2025/e7ff365a-cb59-4ce9-9cdf-4cb965455b69/anim/welcome3/large.mp4"}/>
-      <Products items={Products} />
+      <Products items={products} />
     </>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router';
 
-const Products = (items) => {
+const Products = ({ items }) => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container my-5">
+        <div className="row d-flex justify-content-center">
+          {items.map((product) => <div key={product.id} className='col-lg-4 col-md-6 my-3 d-flex justify-content-center'>
+            <div className="card product-card" style={{ width: '18rem' }}>
+              <Link to={`/product/${product.id}`} className='image-container'>
+                <img src={product.imgSrc} className='card-img-top' alt={product.title} />
+              </Link>
+              <div className="card-body text-center">
+                <h5 className='card-title'>{product.title}</h5>
+                <p className='product-price'>{product.price} ₹</p>
+                <div className="button-container">
+                  <button>
+                    🛒 Add To Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>)}
+        </div>
+      </div>
+
+    </>
   )
 }
 
