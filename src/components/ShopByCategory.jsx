f245c6031c11ea5be18770a92d96a76f63f20c84
Gajanan Gund <gajanangund2000@gmail.com>
2025-10-24 22:01:49 +0530

    Created product filter by price

--- src/components/ShopByCategory.jsx
+++ src/components/ShopByCategory.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { FaMobileAlt, FaLaptop, FaTabletAlt, FaClock, FaThLarge, FaRubleSign } from 'react-icons/fa'
+import { FaMobileAlt, FaLaptop, FaTabletAlt, FaClock, FaThLarge, FaRupeeSign } from 'react-icons/fa'
 import DataContext from '../context/DataContext'
 import { items } from '../context/Data'
 
@@ -18,13 +18,31 @@ const ShopByCategory = () => {
 
   const { setProducts } = useContext(DataContext);
   const [selectedPrice, setSelectedPrice] = useState(80000);
+
+  const filterByCategory = (cat) => {
+    if (cat == "All Products") {
+      setProducts(items)
+      return
+    }
+    setProducts(items.filter(pro => pro.category.toLowerCase() === cat.toLowerCase()))
+  }
+
+  const filterByPrice = (price) =>{
+    setProducts(items.filter(pro=>pro.price <= price))
+  }
   return (
     <div className="container bg-dark text-light my-4 p-4 rounded">
       <h3 className='text-center mb-3'>Filter Products</h3>
       <div className='d-flex flex-wrap justify-content-center align-items-center gap-3 mb-4'>
-        {categories.map(({ name, icons }) => <div key={name} className='d-flex align-items-center gap-2 px-3 py2 rounded bg-secondary text-light fw-bold' style={{ cursor: 'pointer' }}>
+        {categories.map(({ name, icons }) => <div key={name} className='d-flex align-items-center gap-2 px-3 py2 rounded bg-secondary text-light fw-bold' style={{ cursor: 'pointer' }} onClick={() => filterByCategory(name)}>
           {icons} <span>{name}</span>
         </div>)}
+
+      </div>
+      <div className='d-flex flex-wrap justify-content-center gap-3'>
+        {priceRanges.map((value) => <span key={value} className={`badge p-3 fw-bold ${selectedPrice === value ? "bg-warning text-dark" : "bg-light text-dark"}`} style={{ cursor: "pointer", fontSize: "1rem", borderRadius: "10px" }} onClick={() => filterByPrice(value)}>
+          <FaRupeeSign />{value}
+        </span>)}
 
       </div>
 
